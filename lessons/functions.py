d4f2e2d2f63bdaf4487a75b6e30262e2943883e4
chris-reeves1 <chris.reeves@qa.com>
2025-12-01 14:57:07 +0000

    2

---
+++ lessons/functions.py
@@ -0,0 +1,127 @@
+# Repeatability + abstraction 
+
+# def greet(name, age): # params
+#     return f"hello {name} aged: {age}" # use as vars
+
+# print(greet("c", age=20))
+
+# def greet(first, last="B", age=30):
+#     print(f"hello {first}, aged: {age}")
+
+# greet()
+
+# *args 
+# Dont know how many positional args need to be passed through. Passed as a tuple.
+
+# def make_pizza(size, *toppings):
+#     print(F"Order for {size} pizza, with the following topppings:")
+#     for topping in toppings:
+#         print(topping)
+
+# make_pizza("large", "mushrooms", "peppers")
+
+# **kwargs
+# unkown number of key word args
+
+# def order(**stuff):
+#     print("Order:")
+#     for k, v in stuff.items():
+#         print(f"{k} + {v}")
+
+# order(main="pasta", drink="cola", side="fries")
+
+# def combined(*args, **kwargs):
+#     print(args)
+#     print(kwargs)
+
+# combined(1, "a", 3, a=4)
+
+# / * 
+# all args before / must be positional.  
+# All args after * must be keyword args. 
+# Enforced!!
+
+# def example(a, b, /, *, c, d):
+#     print(f"a: {a} b: {b} c: {c} d: {d}")
+
+# example(1, 2, 3, d=4)
+
+# def maths_ops(a, b, /, operation="add", *, decimal_place=None):
+#     if operation == "add":
+#         result = a + b
+#     elif operation == "subtract":
+#         result = a - b
+#     else:
+#         raise TypeError("Only add or subtract pls!!")
+#     return round(result, decimal_place)
+
+# print(maths_ops(2, 5))
+# print(maths_ops(2, 5, "add"))
+# print(maths_ops(2, 5, operation="subtract"))
+# print(maths_ops(2.25651155, 5.3235656887, operation="subtract", decimal_place=5))
+# print(maths_ops(2, 5, 3, 5, 6, 7, operation="subtract"))
+
+
+# Recursion, lambdas, wrappers, functools, higher-levels.
+
+# Recursion:
+
+# def factorial(n):
+#     if n == 1:
+#         return 1
+#     else:
+#        return n * factorial(n - 1)
+
+# print(factorial(5))
+
+# Frame:
+#     - Stores each function call
+#     - locals(), globals().
+#     - code object (f_code)
+#     - calling context
+
+
+# stack:
+#     - stack of frames, ordered from recent to oldest. 
+#     - pops the functions (deletes + returns it return)
+
+
+# import inspect
+
+# def test_function():
+#     print("name: ", inspect.currentframe().f_code.co_name)
+#     print("stack: ", inspect.stack()[0].function)
+#     x = "hello"
+#     print(locals())
+#     for f in inspect.stack():
+#         print(f.function)
+
+# def call():
+#     test_function()
+
+# call()
+# print(locals())
+# print(locals() == globals())
+
+# lambdas:
+
+# discrete one line unnamed(mostly) functions. 
+
+# lambda args : expression (iterables)
+
+# add_function = lambda x , y : x + y
+
+# print(add_function(2, 3))
+
+# numbers = [1, 2, 3, 4, 5]
+# squared = map(lambda x: x**2, numbers)
+
+# print(list(squared))
+
+
+break here till tomoro:
+todo:
+lab 5 and 6
+practice function challenges if needed
+rock, paper, scissor game user vs computer (track stats for wins/rounds/loses etc)
+or... go back through the notes (*args, **kwargs, /, * etc)
--- others/task.py
+++ others/task.py
@@ -8,3 +8,148 @@
 # If he orders but can't pay he's thrown out.
 
 
+# budget = 20
+
+# milkshakes = {
+#     "1": (3, "Whole"),
+#     "2": (4, "choc"),
+#     "3": (5, "vanilla")
+# }
+
+# while True:
+#     print("drinks menu:")
+#     for option, (price, flavour) in milkshakes.items():
+#         print(f"{option} - {flavour} - ${price}")
+
+    
+#     choice = input("enter you choice of drink? ")
+
+#     if choice not in milkshakes:
+#         print("invalid choice")
+#         continue
+
+#     if choice.upper() == "Q":
+#         print("bye")
+#         break
+
+#     price, flavour = milkshakes[choice]
+#     if price > budget:
+#         print("kicked out!")
+#         break
+    
+#     print(f"enjoy {flavour} drink")
+#     budget -= price
+#     print(f"budget is now {budget}")
+
+
+
+# from typing import Dict
+# budget = 20
+# Menu = Dict[str, tuple[int, str]] # type alias - capitalised to show not a var. 
+                                  # Has no affect - python doesnt enforce type ever. 
+                                  # mypy is a static tool to check - best implemented in
+                                  # a static code analysis pipeline.
+                                  # updates to the structure propagtes though all calls. 
+
+# milkshakes: Menu = {
+#      "1": (3, "Whole"),
+#      "2": (4, "choc"),
+#      "3": (5, "vanilla")
+#  }
+
+# while True:
+#     print("drinks menu:")
+#     for option, (price, flavour) in milkshakes.items():
+#         print(f"{option} - {flavour} - ${price}")
+
+    
+#     choice = input("enter you choice of drink? ")
+
+#     if choice not in milkshakes:
+#         print("invalid choice")
+#         continue
+
+#     if choice.upper() == "Q":
+#         print("bye")
+#         break
+
+#     price, flavour = milkshakes[choice]
+#     if price > budget:
+#         print("kicked out!")
+#         break
+    
+#     print(f"enjoy {flavour} drink")
+#     budget -= price
+#     print(f"budget is now {budget}")
+
+# Above example: fails by type and expected size of the tuple. index errors are flagged.
+
+
+
+# # typedDict example:
+
+# from typing import TypedDict, Dict
+
+# class Shake(TypedDict):
+#     price: int
+#     flavour: str
+
+# milkshakes: Dict[str, Shake] = {
+#     "1": {"price": 3, "flavour": "Milk"},
+#     "2": {"price": 5, "flavour": "Choc"},
+#     "3": {"price": 10, "flavour": "Strawberry"},
+# }
+
+# budget = 20
+
+# while True:
+#     print("Menu:")
+#     for option, shake in milkshakes.items():
+#         print(f"{option} - {shake['price']}:{shake['flavour']}")
+
+#     choice = input("choice 1-3 pls or quit(q): ")
+
+#     if choice.lower() == "q":
+#         break
+#     if choice not in milkshakes:
+#         print("invalid choice")
+#         continue
+
+#     price = milkshakes[choice]["price"]
+#     flavour = milkshakes[choice]["flavour"]
+
+#     if price > budget:
+#         print("get out")
+#         break
+
+#     print(f"{flavour} - milkshake served")
+#     budget -= price
+#     print(budget)
+
+# namedTuples and @DataClass also available. 
+
+# from typeguard import typechecked
+# from typing import Dict, Tuple
+
+# @typechecked
+# def create_menu() -> Dict[str, Tuple[int, str]]:
+#     return {
+#         "1": (3, "Milk"),
+#         "2": (5, "Choc"),
+#         "3": (10, "Strawberry"),
+#         "4": ("5", "Vanilla")   #  Runtime TypeError
+#     }
+
+# milkshakes = create_menu()
+
+import sys, typeguard, importlib.metadata
+print("Python", sys.version)
+print("Typeguard:", importlib.metadata.version("typeguard"))
+
+from typeguard import typechecked
+@typechecked
+def boom() -> tuple[int,str]:
+    return ("5","milk")
+
+boom()
+
